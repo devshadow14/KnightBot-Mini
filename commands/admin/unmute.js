65160e8221e088eacc256b3ed2a3c75fbc5c8552
devshadow14 <nd172237@gmail.com>
2026-08-27 01:55:59 +0000

    Update unmute.js

--- commands/admin/unmute.js
+++ commands/admin/unmute.js
@@ -15,10 +15,10 @@ module.exports = {
     async execute(sock, msg, args, extra) {
       try {
         await sock.groupSettingUpdate(extra.from, 'not_announcement');
-        await extra.reply('🔓 Group has been opened!\n\nAll members can send messages now.');
+        await extra.reply('🔓 le group est ouvert!\n\nVous pouvez revenez.');
         
       } catch (error) {
-        await extra.reply(`❌ Error: ${error.message}`);
+        await extra.reply(`❌ Error: ${error.message}`); 
       }
     }
   };
